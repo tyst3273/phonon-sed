4d3961eab494383220ad82a89269dfe613820da7
tyst3273 <tyst3273@colorado.edu>
2019-01-17 11:58:03 -0700

    created SED files

---
+++ calc/sed.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This file is Ty's custom module to for the SED code: keep it in the same
+directory
+
+DATE STAMP: 01.17.2019 MM.DD.YYYY
+"""
+############################################################
+def gsmooth(Raw, win, dom):
+    """
+    This function takes an numpy array defined over some domain and returns
+    a gaussian smoothened version. "Raw" is the input numpy array. "win" is 
+    the width of the gaussian smoothing window used to initialize the 
+    smoothing window; e.g. if you have a signal from 0 to 20 THz and you want
+    to smooth with a window of 1/3 THz, set "win = 1/3.0". dom is the constant
+    spacing between values in the domain; e.g. if you have a numpy array,
+    "freq", ranging from 0 to 20 THz and with length 10000, "dom = freq[1] - 
+    freq[0]" = 0.002 THz. If none of this makes sense, do like I did and
+    figure it our yourself ;)
+    """
+    import numpy as np
+    gwin = round(win*1e12*2*np.pi/dom) #number of array elements in window
+    if gwin % 2 == 0: #make sure its odd sized array
+        gwin = gwin+1
+    if gwin == 1:
+        gauss = np.array([1]) #if array is size 1, convolve with self
+    else:
+        n = 2*np.arange(0,gwin)/(gwin-1)-(1) #centered at 0, sigma = 1
+        n = (3*n) #set width of profile to 6*sigma i.e. end values ~= 0
+        gauss = np.exp(-np.multiply(n,n)/2.0) #gaussian fx to convolve with
+        gauss = gauss/np.sum(gauss) #normalized gaussian
+
+    smooth = np.convolve(Raw,gauss,mode='same')
+    return smooth
+
+####################################################################
+def smoothSED(Raw, win, dom):
+    """
+    Same as gsmooth but smooths 2d array along axis=0; designed for smoothing
+    SED.
+    This function takes an numpy array defined over some domain and returns
+    a gaussian smoothened version. "Raw" is the input numpy array. "win" is 
+    the width of the gaussian smoothing window used to initialize the 
+    smoothing window; e.g. if you have a signal from 0 to 20 THz and you want
+    to smooth with a window of 1/3 THz, set "win = 1/3.0". dom is the constant
+    spacing between values in the domain; e.g. if you have a numpy array,
+    "freq", ranging from 0 to 20 THz and with length 10000, "dom = freq[1] - 
+    freq[0]" = 0.002 THz. If none of this makes sense, do like I did and
+    figure it our yourself ;)
+    """
+    import numpy as np
+    gwin = round(win*1e12*2*np.pi/dom) #number of array elements in window
+    if gwin % 2 == 0: #make sure its odd sized array
+        gwin = gwin+1
+    if gwin == 1:
+        gauss = np.array([1]) #if array is size 1, convolve with self
+    else:
+        n = 2*np.arange(0,gwin)/(gwin-1)-(1) #centered at 0, sigma = 1
+        n = (3*n) #set width of profile to 6*sigma i.e. end values ~= 0
+        gauss = np.exp(-np.multiply(n,n)/2.0) #gaussian fx to convolve with
+        gauss = gauss/np.sum(gauss) #normalized gaussian
+    
+    smooth = np.zeros((len(Raw[:,0]),len(Raw[0,:])))
+    for i in range(len(Raw[0,:])):
+        smooth[:,i] = np.convolve(Raw[:,i],gauss,mode='same')
+    return smooth
+
+############################################################
+def makeKpoints(prim,specialk,dk):
+    """
+    This function takes primitive lattice vectors, prim, and constructs
+    reciprocal lattice vectors from them. It also takes an arbitrarily long
+    (n,3) array of normalized special k points and an integer, dk.
+    Reciprocal lattice vectors are constructed from the primitive lattice 
+    vector and then those reciprocal lattice vectors are projected onto the
+    special k points. An array of k points containing the special k points
+    is then created and dk points between each k point are populated linearly.
+
+    This function returns 2 variables: kpoints and kdist. kpoints is the array
+    containing ((n-1)*dk,3) k points. kdist is cumulative sum of the euclidean
+    k-space distance between each special k point - this is used later for
+    plotting.
+    """
+    import numpy as np
+    kvec = np.zeros((3,3)) #k space vectors 
+    vol = np.dot(prim[0,:],np.cross(prim[1,:],prim[2,:])) #real space cell volume
+    kvec[0,:] = 2*np.pi*np.cross(prim[1,:],prim[2,:])/vol
+    kvec[1,:] = 2*np.pi*np.cross(prim[2,:],prim[0,:])/vol
+    kvec[2,:] = 2*np.pi*np.cross(prim[0,:],prim[1,:])/vol
+    #reciprocal lattice vectors with cubic symmetry are parallel to real 
+    #space vectors
+    for i in range(len(specialk)): #project into reciprocal space 
+        specialk[i,:] = np.dot(kvec,specialk[i,:])
+        
+    nk = (len(specialk)-1)*dk
+    kpoints = np.zeros((nk,3)) 
+    for i in range(len(specialk)-1): #populate space between special k points
+        for j in range(3): #kx, ky, kz
+            kpoints[i*dk:(i+1)*dk,j] = np.linspace(specialk[i,j],
+                    specialk[(i+1),j],dk) 
+            
+    kdist = np.zeros((len(specialk)))
+    for i in range(len(specialk)-1): #euclidean distance between k points
+        kdist[(i+1)] = np.sqrt((specialk[(i+1),0]-specialk[i,0])**2+
+             (specialk[(i+1),1]-specialk[i,1])**2+
+             (specialk[(i+1),2]-specialk[i,2])**2) 
+    kdist = np.cumsum(kdist) #cumulative distance between special k points
+    
+    return [kpoints, kdist]
+
+##########################################################
+def makeTriclinic(n1,n2,n3,lammps='no',element='si'):
+    """
+    See docstring for makeFCCdiamond
+    """
+    import numpy as np
+    import sys
+    import copy as cp
+    
+    if element == 'si':
+        a = 5.431 #Si lattice constant
+        masses = np.array([28.0855,28.0855]) #atomic mass of Si
+    if element == 'ge':
+        a = 5.658 #Ge lattice constant
+        masses = np.array([72.6400,72.6400]) #atomic mass of Ge
+    if element == 'c':
+        a = 3.57 #C lattice constant
+        masses = np.array([12.0107,12.0107]) #atomic mass of C
+    if (n1%2.0 != 0) and (n2%2.0 != 0) and (n3%2.0 != 0):
+        sys.exit('Number of unit cells in each direction must be an even'
+                 ' integer')
+        
+    nT = np.array([n1,n2,n3]).astype(int) #times to translate in each direction
+    unit = np.array(([1,0,0,0], #FCC-diamond unit cell (basis-index,x,y,z,uc)
+                     [2,1,1,1])).astype(float)
+    
+    uc = np.array([0,0])
+    
+    tmp = cp.deepcopy(unit)
+    pos = cp.deepcopy(unit) #basis-index and coordinates of atoms
+    tuc = cp.deepcopy(uc)
+    for i in range(nT[0]-1): #translate in a1-direction 
+        tmp[:,1] = tmp[:,1]+2
+        pos = np.append(pos,tmp,axis=0)
+        tuc = tuc+1
+        uc = np.append(uc,tuc)
+    tmp = cp.deepcopy(pos)
+    tuc = cp.deepcopy(uc)
+    for i in range(nT[1]-1): #translate in a2-direction
+        tmp[:,1] = tmp[:,1]+1
+        tmp[:,2] = tmp[:,2]+3
+        pos = np.append(pos,tmp,axis=0)
+        tuc2 = tuc+max(uc)+1
+        uc = np.append(uc,tuc2)
+    tmp = cp.deepcopy(pos)
+    tuc = cp.deepcopy(uc)
+    for i in range(nT[2]-1): #translate in a3-direction
+        tmp[:,1] = tmp[:,1]+1
+        tmp[:,2] = tmp[:,2]+1
+        tmp[:,3] = tmp[:,3]+4
+        pos = np.append(pos,tmp,axis=0)
+        tuc2 = tuc+max(uc)+1
+        uc = np.append(uc,tuc2)
+    
+    num = len(pos[:,0])
+    ids = np.zeros((num,1))
+    ids[:,0] = np.arange(0,num,1)+1 #will be used later    
+    pos = np.append(ids,pos,axis=1)
+    pos[:,2] = pos[:,2]*a*np.sqrt(2.0)/4.0 #rescale coordinates
+    pos[:,3] = pos[:,3]*a/np.sqrt(24.0) #rescale coordinates
+    pos[:,4] = pos[:,4]*a/np.sqrt(3.0)/4.0 #rescale coordinates
+    
+    if lammps != 'no':
+        with open(lammps, 'w') as fid:
+            xbuff = (pos[np.argwhere(pos[:,3] == 0)[:,0],2][1]-
+                     pos[np.argwhere(pos[:,3] == 0)[:,0],2][0])/2.0
+            xmax = pos[np.argwhere(pos[:,3] == 0)[:,0],2].max()+xbuff
+            xmin = 0-xbuff #by construction
+            ybuff = (np.unique(pos[np.argwhere(pos[:,4] == 0)[:,0],3])[1]-
+                     np.unique(pos[np.argwhere(pos[:,4] == 0)[:,0],3])[0])/2.0
+            ymax = pos[np.argwhere(pos[:,4] == 0)[:,0],3].max()+ybuff
+            ymin = 0-ybuff #by construction
+            zbuff = (np.unique(pos[:,4])[2]-np.unique(pos[:,4])[1])/2.0
+            zmax = pos[:,4].max()+zbuff
+            zmin = 0-zbuff #by construction
+            
+            xy = pos[np.argwhere(pos[:,4] == pos[:,4].max())[:,0],:]
+            xy = xy[np.argwhere(xy[:,3] == xy[:,3].min())[:,0],2].min()
+            xz = xy
+            yz = pos[np.argwhere(pos[:,4] == pos[:,4].max())[:,0],3].min()
+    
+            fid.write(str('LAMMPS DATA FILE\n'))
+        
+            fid.write('\n' + str(len(pos)) + ' atoms\n')
+            fid.write('\n' + str(len(masses)) + ' atom types\n')
+            fid.write('\n' + str(xmin)+' '+str(xmax)+' xlo'+' xhi\n')
+            fid.write(str(ymin)+' '+str(ymax)+' ylo'+' yhi\n')
+            fid.write(str(zmin)+' '+str(zmax)+' zlo'+' zhi\n')
+            fid.write(str(xy)+' '+str(xz)+' '+
+                          str(yz)+' xy xz yz\n')
+            fid.write('\nMasses\n')
+            for i in range(len(masses)):
+                fid.write('\n' + str(i+1) + ' ' + str(float(masses[i])))
+            fid.write('\n\nAtoms\n\n')
+            for i in range(len(pos)-1):
+                fid.write(str(int(i+1)) + ' ' + str(int(pos[i,1])) + ' ' 
+                          + str(pos[i,2]) + ' ' +
+                        str(pos[i,3]) + ' ' + str(pos[i,4]) + '\n')
+            fid.write(str(len(pos)) +  ' ' + str(int(pos[-1,1])) + ' ' 
+                      + str(pos[-1,2]) + ' ' +
+                    str(pos[-1,3]) + ' ' + str(pos[-1,4]))
+            
+    return [num, pos, masses, uc, a] 
+
+##########################################################
+def writeSED(outfile,thz,kpoints,sed,dos):
+    """
+    This function is simple. It writes the frequency data array, k points, 
+    SED matrix, and DOS array to file 'outfile'. Read file with readSED
+    """
+    import numpy as np
+    nf = len(thz)
+    nk = len(kpoints[:,0])
+    sed = np.reshape(sed,(nf*nk,1))
+
+    with open(outfile, 'w') as fid:
+        fid.write('nf = '+str(nf)+'\n')
+        fid.write('nk = '+str(nk)+'\n')
+        for i in range(nf):
+            fid.write(str(thz[i])+'\n')
+        for i in range(nk):
+            fid.write(str(kpoints[i,0])+'\t'+str(kpoints[i,1])+'\t'+
+                      str(kpoints[i,2])+'\n')
+        for i in range(nk*nf):
+            fid.write(str(sed[i,0])+'\n')
+        for i in range(nf):
+            fid.write(str(dos[i,0])+'\n')
+         
+######################################################################
+def readSED(infile):
+    """
+    This function reads in the SED outout file and returns THz, kpoints, SED, 
+    and DOS written to file using writeSED
+    """
+    with open(infile,'r') as fid:
+        import numpy as np
+        nf = int(fid.readline().strip().split()[2])
+        nk = int(fid.readline().strip().split()[2])
+        thz = np.zeros((nf,1))
+        kpoints = np.zeros((nk,3))
+        sed = np.zeros((nf*nk,1)).astype(complex)
+        dos = np.zeros((nf,1))
+        for i in range(nf):
+            thz[i,0] = float(fid.readline())
+        for i in range(nk):
+            kpoints[i,:] = fid.readline().strip().split()[:]
+        kpoints = kpoints.astype(float)
+        for i in range(nf*nk):
+            sed[i,0] = complex(fid.readline())
+        sed = np.reshape(sed,(nf,nk))
+        for i in range(nf):
+            dos[i,0] = float(fid.readline())
+    return thz, kpoints, sed, dos
+
+##########################################################            
+def tic():
+    """
+    Same as MATLAB tic and toc functions. Use ty.tic() at the beginning of
+    code you want to time and ty.toc() at the end. Once ty.toc() is reached,
+    elapsted time will be printed to screen and optionally (by default) written
+    to 'log.txt' file.
+    """
+    import time
+    global startTime_for_tictoc
+    startTime_for_tictoc = time.time()
+
+def toc(logFlag='yes'):
+    """
+    Same as MATLAB tic and toc functions. Use ty.tic() at the beginning of
+    code you want to time and ty.toc() at the end. Once ty.toc() is reached,
+    elapsted time will be printed to screen and optionally (by default) written
+    to 'log.txt' file.
+    """
+    import numpy as np
+    import time
+    if 'startTime_for_tictoc' in globals():
+        if logFlag == 'yes':
+            log("\n\tElapsed time is "+str(np.round(time.time()-
+                           startTime_for_tictoc,decimals=3))+" seconds.")
+        else:
+            print("\n\tElapsed time is "+
+                  str(np.round(time.time()-
+                           startTime_for_tictoc,decimals=3))+" seconds.")
+    else:
+        print("\n\t\tToc: start time not set") 
+        
+        
+##########################################################     
+def vdos(vels,tn,num,dt,dn,win,thz):
+    """
+    This function calculates vibrational density of states from EMD velocity
+    data. Intended to be used adjunct to SED code. 
+    """
+    import numpy as np
+    vels = vels.reshape(tn,num*3)
+    dos = np.zeros((tn,num*3))
+    velsfft = np.fft.fft(vels,axis=0)*dt*dn
+    dos = (np.multiply(abs(velsfft),abs(velsfft))/
+               np.tile(np.multiply(vels,vels).mean(axis=0),(tn,1))/(tn*dt*dn))
+    dosx = gsmooth(dos[:,0::3].mean(axis=1),win,(thz[1]-thz[0])*2*np.pi*1e12)
+    dosy = gsmooth(dos[:,1::3].mean(axis=1),win,(thz[1]-thz[0])*2*np.pi*1e12)
+    dosz = gsmooth(dos[:,2::3].mean(axis=1),win,(thz[1]-thz[0])*2*np.pi*1e12)
+    dos = gsmooth(dos.mean(axis=1),win,(thz[1]-thz[0])*2*np.pi*1e12)
+
+    return [dos, dosx, dosy, dosz]
+
+##########################################################
+def log(string,outfile='log.txt',suppress='no',new='no'):
+    """
+    This function prints output to a file called log.txt and to the screen. 
+    Useful for tracking whats happening when submitted using qsub or slurm etc.
+    If you don't want to print to screen, enter supress='yes'
+    """
+    if new == 'no':
+        with open(outfile,'a') as fid:
+            fid.write(string)
+    else:
+        with open(outfile,'w') as fid:
+            fid.write(string)
+    if suppress == 'no':
+        print(string)
+    
+        
---
+++ deprecated/computeSED-FCCv2.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Date Stamp: 01.08.2019
+
+@author: Ty Sterling
+
+ty.sterling@colorado.edu
+
+LICENSE AGREEMENT: Do whatever you want :)
+
+This was written in python 2.7 installed with conda.
+"""
+
+### GLOBAL VARIABLES ###
+import numpy as np #if it doesnt work, lemme know and ill tell you the versions
+import matplotlib.pyplot as plt
+import ty
+
+ty.tic()
+
+outfile = 'sed.ty.C'
+velsfile = 'vels.dat'
+
+n1, n2, n3 = [16,16,16] #size of simulation cell
+dk = 10 #k space mesh, number of points between speciak k points
+
+steps = 10000 #run time
+dt = 0.3e-15 #lammps time step
+dn = 25 #print frequency
+prints = steps/dn #times data is printed
+split = 2 #times to split data for averaging
+tn = prints/split #timesteps per chunk
+win = 1 #gaussian smoothing window
+pi = np.pi #tired of forgetting the 'np' part...
+
+#om = np.arange(0,tn)*2*np.pi/(tn*dt*dn) #angular frequency
+thz = np.arange(0,tn)/(tn*dt*dn)*1e-12 #frequency in THz
+
+#### GET POSITIONS AND UNITCELL INDICIES ###
+num, pos, masses, uc, a = ty.makeTriclinic(n1,n2,n3,element='c') 
+#get the positions from function
+types = pos[:,1]
+###
+
+#### GET K POINTS ###
+prim = np.array([[2,0,0],
+                 [1,3,0],
+                 [1,1,1]]).astype(float) #primitive lattice vectors
+
+prim[:,0] = prim[:,0]*a*np.sqrt(2.0)/4.0 #a1
+prim[:,1] = prim[:,1]*a/np.sqrt(24.0) #a2
+prim[:,2] = prim[:,2]*a/np.sqrt(3.0) #a3
+
+specialk = np.array([[0,0,0], #gamma
+                     [0.5,0,0.5], #X
+                     [0.375,0.375,0.75], #K
+                     [0,0,0], #gamma
+                     [0.5,0.5,0.5]]) #L 
+                     #special reciprocal lattice points
+klabel = np.array(('G','X','K','G','L')) 
+
+#specialk = np.array([[0,0,0], #gamma
+#                     [0,0.5,0.5], #X
+#                     [0.25,0.5,0.75], #W
+#                     [0.375,0.375,0.75], #K
+#                     [0,0,0], #G
+#                     [0,0,0.5], #L
+#                     [0,0.375,0.625], #U
+#                     [0.25,0.5,0.75], #W
+#                     [0,0,0.5], #L
+#                     [0.375,0.375,0.75]]) #K
+#                     #special reciprocal lattice points
+#klabel = np.array(('G','X','W','K','G','L','U','W','L','K')) 
+
+### FIX THESE COORDS FOR NEW UNIT CELL ?
+
+kpoints, kdist = ty.makeKpoints(prim,specialk,dk) #get the input k space array
+#from a funtion
+
+### GET VELOCITIES AND CALCULATE SED ###
+ty.log('\n\tCALCULATING PHONON SPECTRAL ENERGY DENSITY!',new='yes')
+
+with open(velsfile, 'r') as fid: 
+    nk = len(kpoints) #number of k points
+    ids = np.zeros((num))
+    nc = max(uc)+1
+    nb = len(masses)
+    ids = np.argwhere(types == 1) #atoms in this basis pos
+    cellvec = pos[ids[:,0],:] #coords of fcc basis atom
+    cellvec = cellvec[np.argsort(uc[ids][:,0]),:] #sort by unit cell
+    
+    sed = np.zeros((tn,nk)) #spectral energy density
+    dos = np.zeros((tn,1))
+    
+    #the data is read in in chunks and the chunks are mathed upon until
+    #its a smaller data structure. then the chunks are all block averaged
+    #together. Saves RAM space and it also 'ensemble averages' to 
+    #produce better data      
+          
+    for i in range(1):#split): #loop over chunks to block average
+        ty.log('\n\tNow on chunk: '+str(i+1)+
+              ' out of '+str(split)+'\n')
+        vels = np.zeros((tn,num,3))
+        qdot = np.zeros((tn,nk))
+        
+        #read in vels for this block
+        for j in range(tn): #loop over timesteps in block
+            tmpVels = np.zeros((num,3))
+            for k in range(9): #skip comments
+                fid.readline()
+            for k in range(num): #get atoms
+                tmp = fid.readline().strip().split()
+                vels[j,k,0] = float(tmp[2]) #vx
+                vels[j,k,1] = float(tmp[3]) #vy
+                vels[j,k,2] = float(tmp[4]) #vz
+         
+        #calculate vibrational density of states
+        vdos = ty.vdos(vels,tn,num,dt,dn,win*1.5,thz)
+        dos[:,0] = dos[:,0]+vdos[0][:]
+                
+        #compute SED for this block        
+        for j in range(nk): #loop over all k points
+            kvec = kpoints[j,:] #k point vector
+            if j%50 == 0:
+                ty.log('\t\tNow on k-point: '+str(j)+' out of '+str(nk)+'\n')
+            tmp = np.zeros((tn,1)).astype(complex) #sed for this k point
+            for k in range(nc-1): #loop over unit cells
+                rvec = cellvec[k,2:5] #position of unit cell
+                ids = np.argwhere(uc==k) 
+                for l in range(nb): #loop over basis atoms
+                    mass = masses[l]
+                    vx = vels[:,ids[l,0],0] #time series for particular atom
+                    vy = vels[:,ids[l,0],1] # '' ''
+                    vz = vels[:,ids[l,0],2] # '' ''
+                    
+                    tmp[:,0] = tmp[:,0]+(vx+vy+vz)*np.exp(1j*np.dot(kvec,rvec))
+                    #space fft of velocity data
+                    qdot[:,j] = np.multiply(abs(np.fft.fft(tmp[:,0])),
+                                      abs(np.fft.fft(tmp[:,0])))*mass 
+                    #KE of normal coordinate (square of time-FFT)
+                    
+        sed = sed+np.real(qdot)/(4*np.pi*steps/split*dt*nc) #a buncha constants
+        ty.writeSED(outfile+'.'+str(i)+'.dat',thz,kpoints,sed/(i+1),dos/(i+1))
+        ty.toc() #execution time
+        
+sed = sed/split #average across splits
+dos = dos/split
+
+del cellvec, i, j, k, ids, l, mass, n1, n2, n3, nb, nc, pi, prints, qdot
+del split, steps, tmp, tmpVels, types, uc, vdos, vels, velsfile, vx, vy, vz
+#clean up variables
+
+### WRITE TO A FILE ###
+ty.writeSED(outfile+'.final.dat',thz,kpoints,sed,dos)
+
+sedg = ty.smoothSED(sed,win,(thz[1]-thz[0])*np.pi*1e12)
+#gaussian smooth SED along freq axis for better looking results
+ty.writeSED(outfile+'.smooth.dat',thz,kpoints,sedg,dos)
+
+ty.log('\n\tAll done!')
+
+### PLOT THE DISPERSION CURVE ###
+plt.imshow(sedg,cmap='jet',aspect='auto')
+plt.tight_layout()
+plt.show()
+###
+
